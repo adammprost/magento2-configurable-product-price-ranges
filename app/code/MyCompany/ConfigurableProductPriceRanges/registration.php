d144b3a86d558aff32faaad9e4aad8672aae5df2
Adam Prost <adam.prost@classyllama.com>
2017-09-26 09:41:04 -0500

    Initial Commit

---
+++ app/code/MyCompany/ConfigurableProductPriceRanges/registration.php
@@ -0,0 +1,7 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MyCompany_ConfigurableProductPriceRanges',
+    __DIR__
+);
+?>
